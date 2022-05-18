1142f97dcee28138e068fcfa38dc7f968b2fabbb
彭镜宇 <2101110617@pku.edu.cn>
2022-05-18 13:48:05 +0800

    更新画图

--- diffVel.jl
+++ diffVel.jl
@@ -1,13 +1,13 @@
-"""
-变量单位：
-速度：Km/s
-到太阳距离：Km
-温度：eV
-温度tensor：eV
-数密度：cm-3
 
-"""
+# 变量单位：
+# 速度：Km/s
+# 到太阳距离：Km
+# 温度：eV
+# 温度tensor：eV
+# 数密度：cm-3
 
+
+using Statistics
 using MAT
 using Plots
 using LinearAlgebra
@@ -15,6 +15,7 @@ using Dates
 
 const AU = 149597871 # Km
 
+################ utils ###############
 function loadData()
     pVars = matread("data\\psp_spi_sf00.mat")
     αVars = matread("data\\psp_spi_sf0a.mat")
@@ -44,6 +45,21 @@ function epoch2datetime(epoch::Real)
     unix2datetime((epoch-719529)*86400)
 end
 
+"""
+Vαp VS R
+"""
+function vαpVsR(αVars::Dict,vp::Vector,vαp::Vector;binNum=5,maxR=0.4)
+    R = vec(αVars["alpha_sun_dist"]/AU)
+    binEdges = collect(range(minimum(R),maxR;length=(binNum+1)))
+    Rs = zeros(binNum)
+    vαps = zeros(binNum)
+    for i in 1:binNum
+        Rs[i] = (binEdges[i]+binEdges[i+1])/2
+        vαps[i] = mean(filter(!isnan,vαp[(R.>binEdges[i]) .& (R.<binEdges[i+1])]))
+    end
+    Rs,vαps
+end
+
 pVars,αVars,modifiedVars = loadData()
 # icmeList = matread("list\\PSP_ICME_list.mat")
 sbList = matread("list\\switchback_time_output.mat")
@@ -54,15 +70,26 @@ vα = [αVars["alpha_vel_rtn_sun"][i,:] for i in 1:size(αVars["alpha_vel_rtn_su
 vp = [modifiedVars["p_vel_rtn_sun_alphaEpoch"][i,:] for i in 1:size(modifiedVars["p_vel_rtn_sun_alphaEpoch"])[1]]
 vαp = Vαp.(vα,vp)
 
-#scatter(
-histogram2d(
-αVars["alpha_sun_dist"]/AU,
-vαp,
+rs,vαps = vαpVsR(αVars,vp,vαp;binNum=5)
+
+plot(
+rs,
+vαps,
 xlabel="R [au]",
 ylabel="Vαp [Km/s]",
-# ms=1,
 legend=false,
-ylims=(0,200),
-xlims=(0,0.4),
 )
 savefig("figure\\VαpVsR.png")
+
+#scatter(
+# histogram2d(
+# αVars["alpha_sun_dist"]/AU,
+# vαp,
+# xlabel="R [au]",
+# ylabel="Vαp [Km/s]",
+# # ms=1,
+# legend=false,
+# ylims=(0,200),
+# xlims=(0,0.4),
+# )
+# savefig("figure\\VαpVsR.png")
