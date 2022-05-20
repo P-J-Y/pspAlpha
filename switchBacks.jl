202839a3abe6e33b09d749f7289595f72c3af9df
彭镜宇 <2101110617@pku.edu.cn>
2022-05-20 22:26:30 +0800

    Update switchBacks.jl

--- switchBacks.jl
+++ switchBacks.jl
@@ -1,2 +1,20 @@
 
 using MAT
+
+
+
+function loadList()
+    sbList = matread("list//switchback_time_output.mat")
+end
+
+
+"""
+epoch(matlab's datenum) to julia datetime
+"""
+function epoch2datetime(epoch::Real)
+    unix2datetime((epoch-719529)*86400)
+end
+
+sbList = loadList()
+sbEpochList = sbList["switchback_time_output"]
+sbList = epoch2datetime.(sbEpochList)
